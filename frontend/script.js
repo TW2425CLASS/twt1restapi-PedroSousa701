b6d73da03b0dd13ecf901de8dec77a6b1ef4b330
PedroSousa701 <p.manuel.sousa@ipvc.pt>
2025-06-14 20:22:29 +0100

    ola

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,32 @@
 // JS para operações CRUD com Fetch API
+const alunos = [
+  {
+    nome: "Pedro",
+    apelido: "Sousa",
+    curso: 1,
+    anoCurricular: 2,
+    idade: 21
+  },
+  {
+    id: "05e2",
+    nome: "Manuel",
+    apelido: "Sousa",
+    curso: 2,
+    anoCurricular: 3,
+    idade: 20
+  }
+];
+
+const cursos = [
+  {
+    id: 1,
+    nomeDoCurso: "Engenharia da Computação Gráfica e Multimédia"
+  },
+  {
+    id: 2,
+    nomeDoCurso: "Engenharia de Redes e Sistemas de Computadores"
+  }
+];
+
+renderAlunos();
+renderCursos();
