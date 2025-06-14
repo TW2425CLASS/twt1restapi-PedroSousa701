8ca785d831469a6005c1f13f2b1d9b471fde46ed
PedroSousa701 <p.manuel.sousa@ipvc.pt>
2025-06-14 20:50:21 +0100

    teste1

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,9 @@
 // API real a ser implementada
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://pedromanuelset2003:Pelema_2003@cluster0.fkvwwq9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
